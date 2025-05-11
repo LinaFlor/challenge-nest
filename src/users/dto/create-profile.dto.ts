3314b6d0068646c2b6bbfb3812b5c0a337933b25
Lina Floriani <lina.floriani@rcpo.com.ar>
2025-05-10 22:28:18 -0300

    actualizo mensajes para validaciones

--- src/users/dto/create-profile.dto.ts
+++ src/users/dto/create-profile.dto.ts
@@ -1,11 +1,11 @@
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateProfileDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message: "El codigo debe ser una cadena de texto"})
+  @IsNotEmpty({message: "El codigo no puede estar vacio"})
   codigo: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message: "El nombre del perfil debe ser una cadena de texto"})
+  @IsNotEmpty({message: "El nombre del perfil no puede estar vacio"})
   nombrePerfil: string;
 } 
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,19 +3,20 @@ import { Type } from 'class-transformer';
 import { CreateProfileDto } from './create-profile.dto';
 
 export class CreateUserDto {
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message: "El nombre debe ser una cadena de texto"})
+  @IsNotEmpty({message: "El nombre no puede estar vacio"})
   nombre: string;
 
-  @IsEmail()
-  @IsNotEmpty()
+  @IsEmail({}, {message: "El correo electronico debe ser una direccion de correo valida"})
+  @IsNotEmpty({message: "El correo electronico no puede estar vacio"})
   correoElectronico: string;
 
-  @IsNumber()
-  @IsNotEmpty()
+  @IsNumber({}, {message: "La edad debe ser un numero"})
+  @IsNotEmpty({message: "La edad no puede estar vacia"})
   edad: number;
 
   @ValidateNested()
   @Type(() => CreateProfileDto)
+  @IsNotEmpty({message: "El perfil no puede estar vacio"})
   perfil: CreateProfileDto;
 } 
