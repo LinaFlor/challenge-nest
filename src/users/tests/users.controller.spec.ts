609dbff7e00fc2c24b976f5809ab560b74a1290f
Lina Floriani <lina.floriani@rcpo.com.ar>
2025-05-11 16:22:11 -0300

    agrego tests para services y controllers

---
+++ src/users/tests/users.controller.spec.ts
@@ -0,0 +1,160 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from '../users.controller';
+import { UsersService } from '../users.service';
+import { NotFoundException } from '@nestjs/common';
+import { AuthGuard } from '../../auth/guards/auth.guard';
+import { RolesGuard } from '../../auth/guards/roles.guard';
+
+describe('UsersController', () => {
+  let usersController: UsersController;
+  let usersService: UsersService;
+
+  const mockedUser = {id: 1, nombre: 'Juan Perez', correoElectronico: 'test@example.com', edad: 20, perfil: { nombrePerfil: 'user', id: 1, codigo: 'USR001' }};
+ 
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue([{id: 1, nombre: 'Juan Perez', correoElectronico: 'test@example.com', edad: 20, perfil: { nombrePerfil: 'user', id: 1 }}, {id: 2, nombre: 'Ana Perez', correoElectronico: 'test2@example.com', edad: 21, perfil: { nombrePerfil: 'admin', id: 2 }}]),
+            findOne: jest.fn().mockResolvedValue(mockedUser),
+            create: jest.fn().mockResolvedValue(mockedUser),
+            update: jest.fn().mockResolvedValue(mockedUser),
+            remove: jest.fn().mockResolvedValue(undefined),
+          },
+        },
+        {
+            provide: AuthGuard,
+            useValue: {
+                canActivate: jest.fn(() => true)
+            }
+          },
+          {
+            provide: RolesGuard,
+            useValue: {
+                canActivate: jest.fn(() => true)
+            },
+          },
+      ],
+    }).compile();
+
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(usersController).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create and return a new user', () =>{ 
+
+      const expectedUser = { 
+        id: 1,
+        nombre: 'Juan Perez', 
+        correoElectronico: 'test@example.com', 
+        edad: 20, 
+        perfil: { 
+          nombrePerfil: 'user', 
+          codigo: 'USR001',
+          id: 1
+        } 
+      };
+
+      jest.spyOn(usersService, 'create').mockReturnValue(expectedUser);
+
+      const result = usersController.create(mockedUser);
+      expect(result).toEqual(expectedUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call usersService.findAll with search query if provided', () => {
+        const search = 'juan';
+  
+        jest.spyOn(usersService, 'findAll').mockReturnValue([mockedUser]);
+
+        const result = usersController.findAll(search);
+  
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(usersService.findAll).toHaveBeenCalledWith(search);
+
+        expect(result).toEqual([mockedUser]);
+      });
+    
+    
+    
+    it('should call usersService.findAll with undefined if no search query', () => {
+        const expectedResult = [];
+    
+        jest.spyOn(usersService, 'findAll').mockReturnValue(expectedResult);
+    
+        const result = usersController.findAll();
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(usersService.findAll).toHaveBeenCalledWith(undefined);
+        expect(result).toEqual(expectedResult);
+      });
+    });
+
+  describe('findOne', () => {
+    it('should return a user by id', () => {
+  
+        jest.spyOn(usersService, 'findOne').mockReturnValue(mockedUser);
+  
+        const result = usersController.findOne('1');
+        expect(result).toEqual(mockedUser);
+        // eslint-disable-next-line @typescript-eslint/unbound-method
+        expect(usersService.findOne).toHaveBeenCalledWith(1);
+    });
+  
+    it('should throw an error if user is not found', () => {
+      jest.spyOn(usersService, 'findOne').mockImplementation(() => {
+        throw new NotFoundException('User not found');
+      });
+  
+      expect(() => usersController.findOne('999')).toThrow(NotFoundException);
+    });
+  });
+  
+
+  describe('update', () => {
+    it('should update and return the updated user', () => {
+      const updateUserDto = { nombre: 'Pepe' };
+      const expectedUser = { 
+        id: 1, 
+        nombre: 'Pepe',
+        correoElectronico: 'test@example.com',
+        edad: 20,
+        perfil: { nombrePerfil: 'user', id: 1, codigo: 'USR001' }
+      };
+  
+      jest.spyOn(usersService, 'update').mockReturnValue(expectedUser);
+  
+      const result = usersController.update('1', updateUserDto);
+      
+      expect(result).toEqual(expectedUser);
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(usersService.update).toHaveBeenCalledWith(1, updateUserDto);
+    });
+  });
+  
+
+  describe('remove', () => {
+    it('should remove a user', async () => {
+      await expect(usersController.remove('1')).resolves.not.toThrow();
+    });
+  
+    it('should throw an error if user is not found', () => {
+      jest.spyOn(usersService, 'remove').mockImplementation(() => {
+        throw new NotFoundException('Usuario no encontrado');
+      });
+  
+      expect(() => usersController.remove('999')).toThrow(NotFoundException);
+    });
+  });
+  
+  
+});
+
---
+++ src/users/tests/users.services.spec.ts
@@ -0,0 +1,159 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users.service';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+describe('UsersService', () => {
+  let usersService: UsersService;
+  const mockedUser = {
+    nombre: 'Juan Perez',
+    correoElectronico: 'juan.perez@example.com',
+    edad: 30,
+    perfil: { nombrePerfil: 'user', codigo: 'USR001', id: 1},
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService],
+    }).compile();
+
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(usersService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create and return a new user', () => {
+  
+      const result = usersService.create(mockedUser);
+  
+      expect(result).toEqual({
+        id: 1,
+        ...mockedUser,
+        perfil: {...mockedUser.perfil },
+      });
+    });
+
+    it('should throw an error if any required field is missing', () => {
+        const createUserDto = {
+          // El campo 'nombre' está ausente
+          correoElectronico: 'juan.perez@example.com',
+          edad: 30,
+          perfil: { nombrePerfil: 'user', codigo: 'USR001', id: 1 },
+        };
+    
+        //@ts-expect-error - estamos forzando la falta del campo nombre
+        expect(() => usersService.create(createUserDto)).toThrow(
+          new BadRequestException('Faltan campos requeridos: nombre')
+        );
+      });
+  
+    it('should throw an error if the email is already in use', () => {
+  
+      usersService.create(mockedUser);
+
+      expect(() => usersService.create(mockedUser)).toThrow(
+        new BadRequestException(
+          'El correo electrónico juan.perez@example.com ya está en uso.'
+        )
+      );
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return all users if no search query is provided', () => {
+      const createUserDto1 = { nombre: 'Juan', correoElectronico: 'juan@example.com', edad: 30, perfil: { nombrePerfil: 'user', codigo: 'USR001' } };
+      const createUserDto2 = { nombre: 'Maria', correoElectronico: 'maria@example.com', edad: 25, perfil: { nombrePerfil: 'admin', codigo: 'ADM001' } };
+  
+      usersService.create(createUserDto1);
+      usersService.create(createUserDto2);
+  
+      const result = usersService.findAll();
+      expect(result.length).toBe(2);
+      expect(result).toEqual([
+        {
+          ...createUserDto1,
+          id: 1,
+          perfil: {
+            ...createUserDto1.perfil,
+            id: 1
+          }
+        },
+        {
+          ...createUserDto2,
+          id: 2,
+          perfil: {
+            ...createUserDto2.perfil,
+            id: 2
+          }
+        }
+      ]);
+    });
+  
+    it('should return filtered users based on search query', () => {
+      const createUserDto1 = { nombre: 'Juan', correoElectronico: 'juan@example.com', edad: 30, perfil: { nombrePerfil: 'user', codigo: 'USR001', id: 1 } };
+      const createUserDto2 = { nombre: 'Maria', correoElectronico: 'maria@example.com', edad: 25, perfil: { nombrePerfil: 'admin', codigo: 'ADM001', id: 2 } };
+  
+      usersService.create(createUserDto1);
+      usersService.create(createUserDto2);
+  
+      const result = usersService.findAll('juan');
+      expect(result.length).toBe(1);
+      expect(result[0].nombre).toBe('Juan');
+    });
+  });
+  
+  describe('findOne', () => {
+    it('should return a user by id', () => {  
+      const newUser = usersService.create(mockedUser);
+  
+      const result = usersService.findOne(newUser.id);
+      expect(result).toEqual(newUser);
+    });
+  
+    it('should throw NotFoundException if the user is not found', () => {
+      expect(() => usersService.findOne(999)).toThrow(
+        new NotFoundException('Usuario con ID 999 no encontrado')
+      );
+    });
+  });
+
+  describe('update', () => {
+    it('should update and return the user', () => {  
+      const newUser = usersService.create(mockedUser);
+  
+      const updateUserDto = { nombre: 'Pepe' };
+      const updatedUser = usersService.update(newUser.id, updateUserDto);
+  
+      expect(updatedUser.nombre).toBe('Pepe');
+      expect(updatedUser.id).toBe(newUser.id);
+    });
+  
+    it('should throw NotFoundException if the user to update is not found', () => {
+      const updateUserDto = { nombre: 'Pepe' };
+  
+      expect(() => usersService.update(999, updateUserDto)).toThrow(
+        new NotFoundException('Usuario con ID 999 no encontrado')
+      );
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove the user', () => {  
+      const newUser = usersService.create(mockedUser);
+  
+      usersService.remove(newUser.id);
+      expect(() => usersService.findOne(newUser.id)).toThrow(
+        new NotFoundException(`Usuario con ID ${newUser.id} no encontrado`)
+      );
+    });
+  
+    it('should throw NotFoundException if the user to remove is not found', () => {
+      expect(() => usersService.remove(999)).toThrow(
+        new NotFoundException('Usuario con ID 999 no encontrado')
+      );
+    });
+  });
+  
+});
